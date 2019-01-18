c4f12a14b6c03f542ea9ed9581160d7519974027
Rebecca Chan <chan.rebecca@gmail.com>
2019-01-18 12:22:21 -0500

    first commit

--- client/index.js
+++ client/index.js
@@ -1,19 +1,174 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import {Provider} from 'react-redux'
-import {Router} from 'react-router-dom'
-import history from './history'
-import store from './store'
-import App from './app'
+const langs = require('./webspeechLangs')
+const outputLanguages = require('./translationLangs')
+const API_KEY = require('../secrets')
 
-// establishes socket connection
-import './socket'
+for (let i = 0; i < outputLanguages.length; i++) {
+  output_language.options[i] = new Option(
+    outputLanguages[i].name,
+    outputLanguages[i].language
+  )
+}
 
-ReactDOM.render(
-  <Provider store={store}>
-    <Router history={history}>
-      <App />
-    </Router>
-  </Provider>,
-  document.getElementById('app')
-)
+const getTranslation = async final_transcript => {
+  let targetLang = output_language.value
+  const uri = `https://translation.googleapis.com/language/translate/v2?target=${targetLang}&key=${API_KEY}&q=${final_transcript}`
+  console.log(targetLang, 'targetLang')
+  const response = await fetch(uri)
+  const {data} = await response.json()
+  translated_span.innerHTML = data.translations[0].translatedText
+}
+
+//creates options for every input language in webSpeech API
+for (let i = 0; i < langs.length; i++) {
+  select_language.options[i] = new Option(langs[i][0], i)
+}
+select_language.selectedIndex = 6
+updateCountry()
+select_dialect.selectedIndex = 6
+showInfo('info_start')
+
+//allows you to select country based on selected language
+function updateCountry() {
+  for (var i = select_dialect.options.length - 1; i >= 0; i--) {
+    select_dialect.remove(i)
+  }
+  var list = langs[select_language.selectedIndex]
+  for (var i = 1; i < list.length; i++) {
+    select_dialect.options.add(new Option(list[i][1], list[i][0]))
+  }
+  select_dialect.style.visibility = list[1].length == 1 ? 'hidden' : 'visible'
+}
+
+let final_transcript = ''
+let recognizing = false
+let ignore_onend
+let start_timestamp
+if (!('webkitSpeechRecognition' in window)) {
+  upgrade()
+} else {
+  start_button.style.display = 'inline-block'
+  var recognition = new webkitSpeechRecognition()
+  recognition.continuous = true
+  recognition.interimResults = true
+
+  recognition.onstart = function() {
+    recognizing = true
+    showInfo('info_speak_now')
+    start_img.src = 'recording_nopause.gif'
+  }
+
+  recognition.onerror = function(event) {
+    if (event.error == 'no-speech') {
+      start_img.src = 'recording.png'
+      showInfo('info_no_speech')
+      ignore_onend = true
+    }
+    if (event.error == 'audio-capture') {
+      start_img.src = 'recording.png'
+      showInfo('info_no_microphone')
+      ignore_onend = true
+    }
+    if (event.error == 'not-allowed') {
+      if (event.timeStamp - start_timestamp < 100) {
+        showInfo('info_blocked')
+      } else {
+        showInfo('info_denied')
+      }
+      ignore_onend = true
+    }
+  }
+
+  recognition.onend = function() {
+    recognizing = false
+    if (ignore_onend) {
+      return
+    }
+    start_img.src = 'recording.png'
+    if (!final_transcript) {
+      showInfo('info_start')
+      return
+    }
+    showInfo('')
+    if (window.getSelection) {
+      window.getSelection().removeAllRanges()
+      var range = document.createRange()
+      range.selectNode(document.getElementById('final_span'))
+      window.getSelection().addRange(range)
+    }
+  }
+
+  recognition.onresult = function(event) {
+    let interim_transcript = ''
+    for (let i = event.resultIndex; i < event.results.length; ++i) {
+      if (event.results[i].isFinal) {
+        final_transcript += event.results[i][0].transcript
+      } else {
+        interim_transcript += event.results[i][0].transcript
+      }
+    }
+    final_transcript = capitalize(final_transcript)
+    const translatedText = getTranslation(interim_transcript)
+    console.log('translatedText', translatedText)
+    if (translatedText) translated_span.innerHTML = translatedText
+    if (final_transcript) getTranslation(final_transcript)
+    translated_span.innerHTML = getTranslation(final_transcript)
+    final_span.innerHTML = linebreak(final_transcript)
+    interim_span.innerHTML = linebreak(interim_transcript)
+  }
+}
+
+function upgrade() {
+  start_button.style.visibility = 'hidden'
+  showInfo('info_upgrade')
+}
+
+var two_line = /\n\n/g
+var one_line = /\n/g
+function linebreak(s) {
+  return s.replace(two_line, '<p></p>').replace(one_line, '<br>')
+}
+
+var first_char = /\S/
+function capitalize(s) {
+  return s.replace(first_char, function(m) {
+    return m.toUpperCase()
+  })
+}
+
+document.getElementById('start_button').addEventListener('click', () => {
+  if (recognizing) {
+    recognition.stop()
+    return
+  }
+  final_transcript = ''
+  recognition.lang = select_dialect.value
+  recognition.start()
+  ignore_onend = false
+  final_span.innerHTML = ''
+  interim_span.innerHTML = ''
+  start_img.src = 'recording_nopause.gif'
+  showInfo('info_allow')
+  showButtons('none')
+  start_timestamp = event.timeStamp
+})
+
+function showInfo(s) {
+  if (s) {
+    for (var child = info.firstChild; child; child = child.nextSibling) {
+      if (child.style) {
+        child.style.display = child.id == s ? 'inline' : 'none'
+      }
+    }
+    info.style.visibility = 'visible'
+  } else {
+    info.style.visibility = 'hidden'
+  }
+}
+
+var current_style
+function showButtons(style) {
+  if (style == current_style) {
+    return
+  }
+  current_style = style
+}
