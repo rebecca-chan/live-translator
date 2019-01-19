aab845d2c8b5f28db1773c02b6a891307d788d50
Rebecca Chan <chan.rebecca@gmail.com>
2019-01-19 16:15:49 -0500

    added port var for herok deployment

---
+++ server/index.js
@@ -0,0 +1,46 @@
+const path = require('path')
+const compression = require('compression')
+const PORT = process.env.PORT || 8080
+const express = require('express')
+const app = express()
+
+module.exports = app
+
+if (process.env.NODE_ENV !== 'production') require('../secrets')
+
+const createApp = () => {
+  //body parsing
+  app.use(express.json())
+  app.use(express.urlencoded({extended: true}))
+
+  app.use(express.static(path.join(__dirname, '..', 'public')))
+
+  //send everthing ot index.html
+  app.use('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '..', 'public/index.html'))
+  })
+
+  // compression middleware
+  app.use(compression())
+}
+
+const startListening = () => {
+  // start listening (and create a 'server' object representing our server)
+  const server = app.listen(PORT, () =>
+    console.log(`Mixing it up on port ${PORT}`)
+  )
+}
+
+async function bootApp() {
+  await createApp()
+  await startListening()
+}
+// This evaluates as true when this file is run directly from the command line,
+// i.e. when we say 'node server/index.js' (or 'nodemon server/index.js', or 'nodemon server', etc)
+// It will evaluate false when this module is required by another module - for example,
+// if we wanted to require our app in a test spec
+if (require.main === module) {
+  bootApp()
+} else {
+  createApp()
+}
